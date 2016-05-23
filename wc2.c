53d7f5078c206997b8e12938c42339f7964715ad
LUIS FELIPE OLIVEIRA DE CASTRO <16111208@l1801273.portoalegre.pucrsnet.br>
2016-05-23 10:55:49 -0300

    #7

--- wc2.c
+++ wc2.c
@@ -10,31 +10,31 @@ int main(int argc, char* argv[]) {
     int cp = 0;
     int flag = 1;
 
-    arquivo = fopen(argv[1], "r");
+    arquivo = fopen(argv[1], "r");/*abrir arquivo digitado por linha de comando*/
 
-    c = getc(arquivo);
+    c = getc(arquivo);/*pega o primeiro byte*/
     while( c != -1 )
     {
-        cb++;
-        if (c == '\n')
+        cb++;/*conta o numero de bytes*/
+        if (c == '\n')/*conta as linhas*/
             cl = cl + 1;
-        if ((c == ' ')||(c == '\n')||(c == '\t')){
-		if (flag == 0){
-			cp = cp + 1;
-			flag = 1;
-		}
+        if ((c == ' ')||(c == '\n')||(c == '\t')){/*checa se está entre palavras*/
+			if (flag == 0){/*checa se já não estave entre palavras*/
+				cp = cp + 1;/*conta as palavras*/
+				flag = 1;/*indica que já esta entre palavras*/
+			}
 	}else{
-		flag = 0;		
+		flag = 0;	/*indica que não está mais entre palavras*/	
 	}
 
         /*printf("%i %c %x\n", c, c, c);*/
-        c = getc(arquivo);
+        c = getc(arquivo);/*pega o próximo byte*/
     }
 
-    printf("L=%3i P=%3i B=%3i\n", cl, cp, cb);
+    printf("L=%3i P=%3i B=%3i\n", cl, cp, cb);/*escreve na tela o numero de linha, palavras e bytes*/
 
 
-    fclose(arquivo);
+    fclose(arquivo);/*fecha o arquivo*/
 
 
     return 0;
